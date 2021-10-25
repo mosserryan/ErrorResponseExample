c916d4e18a106b11de595d5eb79b517ebdeb194f
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-10-25 12:01:21 -0400

    Initial commit

---
+++ ErrorResponseExample/src/main/java/com/appsdeveloperblog/aws/errorresponse/lambda/DivisionExampleFunction.java
@@ -0,0 +1,38 @@
+package com.appsdeveloperblog.aws.errorresponse.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class DivisionExampleFunction implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+    
+        Map<String, String> queryStringParameters = input.getQueryStringParameters();
+        
+        int dividend = Integer.parseInt(queryStringParameters.get("dividend"));
+        int divisor = Integer.parseInt(queryStringParameters.get("divisor"));
+ 
+        response.withStatusCode(200).withBody(
+                "{" 
+                        + "\"dividend\":" + dividend + ","
+                        + "\"divisor\":" + divisor +
+                "}"
+         );
+        
+        return response;
+
+    }
+
+}
---
+++ ErrorResponseExample/src/test/java/com/appsdeveloperblog/aws/errorresponse/lambda/DivisionExampleFunctionTest.java
@@ -0,0 +1,21 @@
+package com.appsdeveloperblog.aws.errorresponse.lambda;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class DivisionExampleFunctionTest {
+  @Test
+  public void successfulResponse() {
+//    DivisionExampleFunction app = new DivisionExampleFunction();
+//    APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
+//    assertEquals(result.getStatusCode().intValue(), 200);
+//    assertEquals(result.getHeaders().get("Content-Type"), "application/json");
+//    String content = result.getBody();
+//    assertNotNull(content);
+//    assertTrue(content.contains("\"message\""));
+//    assertTrue(content.contains("\"hello world\""));
+//    assertTrue(content.contains("\"location\""));
+  }
+}
